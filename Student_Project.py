9f52539f41931943445bf3a65e4e01a59eeb7b22
Agochar123 <94464716+Agochar123@users.noreply.github.com>
2021-12-29 19:44:49 +0530

    Update Student_Project.py

--- Student_Project.py
+++ Student_Project.py
@@ -1,1 +1,8 @@
-#Write the code here
+marks = int(input('enter your marks here'))
+if marks>=80:
+    print("excellent job")
+elif marks>60:
+    print("good job")
+elif marks>60:
+    print("need to hard work")    
+
